a368a3b8244e9561b696714b13ce557114d875b8
hernan527 <you@example.comhernan527@gmail.com>
2021-11-07 05:54:47 -0300

    commit homework hasa donde llegue antes del HC

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,25 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+var nombreMay = nombre.charAt(0).toUpperCase() + nombre.slice(1)
+return nombreMay;
+
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-}
+
+cb();
+
+  }
+
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,7 +30,14 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var acumulador = 0
+var sumNumeros = numeros.reduce(function(acumulador,elemento)
+{return acumulador + elemento})
+
+return cb(sumNumeros)
 }
+
+
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
